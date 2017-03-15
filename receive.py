4ce824943cd5a087158908e250569770d3ca49c1
Jacky Lu <jackylu@berkeley.edu>
2017-03-14 18:11:36 -0700

    added receive function

--- receive.py
+++ receive.py
@@ -4,3 +4,8 @@ wind_fir_low = scipy.signal.firwin(511, [35.0 * 2/ mod_sample_rate, 65.0  * 2/ m
 wind_fir_high = scipy.signal.firwin(511, [135.0  * 2/ mod_sample_rate, 165.0  * 2/ mod_sample_rate], pass_zero=False)
 
 low_pass_coeff = scipy.signal.firwin(127, bit_rate / mod_sample_rate)
+
+def receive(duration):
+    frames_per_second = 48000
+    recording = sd.rec(int(duration * frames_per_second), samplerate=frames_per_second)
+    return recording
